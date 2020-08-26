ea041b9ad89f966071347f63cf24ce3795c3497a
Binita72 <70279389+Binita72@users.noreply.github.com>
2020-08-26 21:10:01 +0530

    Add files via upload

---
+++ Basics.R
@@ -0,0 +1,50 @@
+
+
+x <- 58
+y <-20
+z<-100
+>1+1
+1+1
+2+3*4
+exp(1)
+sqrt(10)
+pi
+2*pi*6378
+x<-12
+x<-c(1,205,4.5)
+#create a vector
+
+vect_x<-c(2,0,0,4)
+vect_y<-c(1,9,9,9)
+vect_add<-vect_x+vect_y
+vect_x*5
+#accessing the elements ofthe vector
+vect_x[1]
+vect_x[4]
+#change the element of vetcor
+vect_x[2]<- 2
+vect_x
+#how to deal with characters
+vect_char<-c('R', 'python', 'java', 10, 100.5)
+
+
+
+
+df<-data.frame(x=1:3, y=c("a", "b", "c"))
+df
+df<-data.frame(x=1.3, y=c("a", "b", "c"))
+df[1,1]
+df[x,1]
+df[1,2]
+df[1,1:1,2]
+df[3,1]
+df[1,2]
+df[1,2:3,2]
+df[1,]
+df<-data.frame(x=1, 2, 3, y=c("a", "b", "c"))
+df
+df[1,x]
+df[1,1,2]
+df[1, c(1,2)]
+df<-data.frame(x=1.3, y=c("a", "b", "c"))
+df
